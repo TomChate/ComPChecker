ab6df469eebd35fdcb0125ab4ef6c51d74b29237
TomChate <up774061@myport.ac.uk>
2017-01-18 10:15:10 +0000

    Created Java Program
    Created java program and created UserAccount Class

---
+++ src/main/UserAccount.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Tom
+ */
+public class UserAccount {
+    private String username;
+    private String password;
+    private String Fname;
+    private String Sname;
+    private String email;
+    private char type;          //Type of User 'A' for Admin, 'G' for general.
+    
+     public static void getUser() {
+  
+    }
+    
+}
+
+

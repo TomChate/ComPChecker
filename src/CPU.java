3d4fa267ded450b3edfd9316ce1b45eddf227b60
Tom Chate <up774061@myport.ac.uk>
2017-02-03 11:37:41 +0000

    Save CPU to database

--- src/CPU.java
+++ src/CPU.java
@@ -10,72 +10,81 @@ import java.sql.Statement;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Tom
  */
 public class CPU {
+
     String make;
     String model;
-   double price;
+    double price;
     float speed;
     int cores;
     boolean graphics;
-    
-    
-    
-    
-    public void setMake(String make){
-    
-    this.make = make;
+
+    public void setMake(String make) {
+
+        this.make = make;
     }
 
-    public void setSpeed(float speed){
-    this.speed = speed;
-    
-    }
-    
-    public void setCores(int cores){
-     this.cores = cores;   
-        
-    
-    }
-     public void setGraphics(boolean graphics){
-     this.graphics = graphics;
-        
+    public void setSpeed(float speed) {
+        this.speed = speed;
+
     }
 
-    public boolean saveCPU(){
-        
-     Connection con = DatabaseConnection.establishConnection();
-    
-    try {
-       String query = "INSERT INTO Part  (Price,Model,Make,PartType) VALUES (?,?,?,?)"; 
-          
-       PreparedStatement statement = con.prepareStatement(query);
-    
+    public void setCores(int cores) {
+        this.cores = cores;
 
-       statement.setDouble(1, this.price);
-       statement.setString(2, this.model);
-       statement.setString(3, this.make);
-       statement.setString(4,"CPU");
-       statement.execute();
-       Statement stmt = (Statement) con.createStatement();
-       query =("SELECT ID FROM Part WHERE Model='"+this.model+ "'AND Make='"+this.make+"' AND PartType='CPU'   ");
-       stmt.executeQuery(query);
-       ResultSet rs = stmt.getResultSet();
-       return true;
     }
-    catch(SQLException err){
-        return false;
-        
-    
+
+    public void setGraphics(boolean graphics) {
+        this.graphics = graphics;
+
     }
-    
+
+    public boolean saveCPU() {
+
+        Connection con = DatabaseConnection.establishConnection();
+
+        try {
+            String query = "INSERT INTO Part  (Price,Model,Make,PartType) VALUES (?,?,?,?)";
+
+            PreparedStatement statement = con.prepareStatement(query);
+
+            statement.setDouble(1, this.price);
+            statement.setString(2, this.model);
+            statement.setString(3, this.make);
+            statement.setString(4, "CPU");
+            statement.execute();
+            String model = this.model;
+            query = "SELECT * FROM Part WHERE Model ='" + model + "' && PartType = 'CPU'";
+
+            statement.executeQuery(query);
+            ResultSet rs = statement.getResultSet();
+            int partID = 0;
+            while (rs.next()) {
+                partID = rs.getInt("PartID");
+            }
+
+            query = "INSERT INTO CPU values (?,?,?,?)";
+
+            statement = con.prepareStatement(query);
+
+            statement.setInt(1, partID);
+            statement.setFloat(2, this.speed);
+            statement.setInt(3, this.cores);
+            statement.setInt(4, 1);
+            statement.execute();
+
+
+            return true;
+
+        } catch (SQLException err) {
+            return false;
+
+        }
+
     }
-    
-    
-    
-    }
-    
+
+}

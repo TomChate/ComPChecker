a23f99a4d326c6b7e6e6d4c0e7cbba4870214a52
Tom Chate <up774061@myport.ac.uk>
2017-02-03 13:46:11 +0000

    Added Motherboard, and its GUI
    Doesn't work right now.

--- src/AddMotherboard.java
+++ src/AddMotherboard.java
@@ -41,6 +41,8 @@ public class AddMotherboard extends javax.swing.JDialog {
     private void initComponents() {
 
         cmboxSocket1 = new javax.swing.JComboBox<>();
+        txtboxSlots1 = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
         cmboxMake = new javax.swing.JComboBox<>();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -53,6 +55,12 @@ public class AddMotherboard extends javax.swing.JDialog {
         jLabel4 = new javax.swing.JLabel();
         cmboxSocket = new javax.swing.JComboBox<>();
         cmboxFormFactor = new javax.swing.JComboBox<>();
+        txtboxSlots = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        cmboxRAMType = new javax.swing.JComboBox<>();
+        jLabel10 = new javax.swing.JLabel();
+        txtboxmaxRAM = new javax.swing.JTextField();
 
         cmboxSocket1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cmboxSocket1.addActionListener(new java.awt.event.ActionListener() {
@@ -60,6 +68,8 @@ public class AddMotherboard extends javax.swing.JDialog {
                 cmboxSocket1ActionPerformed(evt);
             }
         });
+
+        jLabel8.setText("RAM Slots:");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -104,28 +114,57 @@ public class AddMotherboard extends javax.swing.JDialog {
             }
         });
 
+        jLabel7.setText("RAM Slots:");
+
+        jLabel9.setText("Ram Type:");
+
+        cmboxRAMType.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cmboxRAMType.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmboxRAMTypeActionPerformed(evt);
+            }
+        });
+
+        jLabel10.setText("Max RAM:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(82, 82, 82)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel6)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel5))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
-                    .addComponent(btnSave, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(cmboxMake, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(txtboxModel)
-                    .addComponent(txtboxPrice)
-                    .addComponent(cmboxSocket, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(cmboxFormFactor, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(76, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(312, 312, 312)
+                        .addComponent(btnSave, javax.swing.GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(82, 82, 82)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel6)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel1)
+                                    .addComponent(jLabel5)))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel9, javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel7, javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel10, javax.swing.GroupLayout.Alignment.TRAILING))))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+                            .addComponent(cmboxMake, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtboxModel)
+                            .addComponent(txtboxPrice)
+                            .addComponent(cmboxSocket, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(cmboxFormFactor, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(cmboxRAMType, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtboxSlots)
+                            .addComponent(txtboxmaxRAM, javax.swing.GroupLayout.Alignment.TRAILING))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -144,7 +183,7 @@ public class AddMotherboard extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
                     .addComponent(txtboxPrice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(cmboxSocket, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -152,7 +191,19 @@ public class AddMotherboard extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(cmboxFormFactor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(6, 6, 6)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel9)
+                    .addComponent(cmboxRAMType, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(txtboxSlots, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel10)
+                    .addComponent(txtboxmaxRAM, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
                 .addComponent(btnSave)
                 .addGap(51, 51, 51))
         );
@@ -167,6 +218,27 @@ public class AddMotherboard extends javax.swing.JDialog {
     private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
         // TODO add your handling code here:
 
+        Motherboard motherboard = new Motherboard();
+
+        String make = cmboxMake.getSelectedItem().toString();
+        String model = txtboxModel.getText();
+        double price = Double.parseDouble(txtboxPrice.getText());
+        String socket = cmboxSocket.getSelectedItem().toString();
+        String size = cmboxFormFactor.getSelectedItem().toString();
+        int slots = Integer.parseInt(txtboxSlots.getText());
+        int maxRAM = Integer.parseInt(txtboxmaxRAM.getText());
+
+        motherboard.setMake(make);
+        motherboard.setModel(model);
+        motherboard.setPrice(price);
+        motherboard.setSize(size);
+        motherboard.setSocket(socket);
+        motherboard.setRamSlots(slots);
+        motherboard.setMaxRAM(maxRAM);
+        
+        motherboard.saveMotherboard();
+
+
     }//GEN-LAST:event_btnSaveActionPerformed
 
     private void cmboxSocketActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmboxSocketActionPerformed
@@ -180,6 +252,10 @@ public class AddMotherboard extends javax.swing.JDialog {
     private void cmboxFormFactorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmboxFormFactorActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_cmboxFormFactorActionPerformed
+
+    private void cmboxRAMTypeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmboxRAMTypeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cmboxRAMTypeActionPerformed
 
     private void populateComboBoxes() {
 
@@ -203,32 +279,32 @@ public class AddMotherboard extends javax.swing.JDialog {
         sockets.add("LGA2011-3");
         sockets.add("AM3+/AM3");
         sockets.add("FM2+");
-        
-        
-       for(int i = 0; i < sockets.size(); i++) {
-         cmboxSocket.addItem(sockets.get(i));
-}
-       cmboxFormFactor.removeAllItems();
-       ArrayList<String> formfactor = new ArrayList<String>();
+
+        for (int i = 0; i < sockets.size(); i++) {
+            cmboxSocket.addItem(sockets.get(i));
+        }
+        cmboxFormFactor.removeAllItems();
+        ArrayList<String> formfactor = new ArrayList<String>();
         formfactor.add("ATX");
         formfactor.add("EATX");
         formfactor.add("Flex ATX");
-         formfactor.add("HTPX");
-         formfactor.add("Micro ATX");
-         formfactor.add("Mini DTX");
-         formfactor.add("Mini ITX");
-         formfactor.add("SSI CEB");
-         formfactor.add("SSI EEB");
-         formfactor.add("Thin Mini ITX");
-         formfactor.add("EX ATX");
-         
-         
-         
-           for(int i = 0; i < formfactor.size(); i++) {
-         cmboxFormFactor.addItem(formfactor.get(i));
-}
+        formfactor.add("HTPX");
+        formfactor.add("Micro ATX");
+        formfactor.add("Mini DTX");
+        formfactor.add("Mini ITX");
+        formfactor.add("SSI CEB");
+        formfactor.add("SSI EEB");
+        formfactor.add("Thin Mini ITX");
+        formfactor.add("EX ATX");
+
+        for (int i = 0; i < formfactor.size(); i++) {
+            cmboxFormFactor.addItem(formfactor.get(i));
+        }
+        cmboxRAMType.removeAllItems();
+        cmboxRAMType.addItem("DDR2");
+        cmboxRAMType.addItem("DDR3");
+        cmboxRAMType.addItem("DDR4");
     }
-    
 
     /**
      * @param args the command line arguments
@@ -277,15 +353,23 @@ public class AddMotherboard extends javax.swing.JDialog {
     private javax.swing.JButton btnSave;
     private javax.swing.JComboBox<String> cmboxFormFactor;
     private javax.swing.JComboBox<String> cmboxMake;
+    private javax.swing.JComboBox<String> cmboxRAMType;
     private javax.swing.JComboBox<String> cmboxSocket;
     private javax.swing.JComboBox<String> cmboxSocket1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JTextField txtboxModel;
     private javax.swing.JTextField txtboxPrice;
+    private javax.swing.JTextField txtboxSlots;
+    private javax.swing.JTextField txtboxSlots1;
+    private javax.swing.JTextField txtboxmaxRAM;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/Motherboard.java
@@ -0,0 +1,126 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Tom
+ */
+public class Motherboard {
+
+    String make;
+    String model;
+    double price;
+    String size;
+    String socket;
+    int ramSlots;
+    int maxRAM;
+
+    /**
+     *
+     * @param make
+     */
+    public void setMake(String make) {
+
+        this.make = make;
+    }
+
+    /**
+     *
+     * @param socket
+     */
+    public void setSocket(String socket) {
+        this.socket = socket;
+    }
+
+    /**
+     *
+     * @param model
+     */
+    public void setModel(String model) {
+        this.model = model;
+
+    }
+
+    /**
+     *
+     * @param price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+
+    }
+
+    /**
+     *
+     * @param size
+     */
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    /**
+     *
+     * @param slots
+     */
+    public void setRamSlots(int slots) {
+        this.ramSlots =slots;
+    }
+
+  
+        
+    /**
+     *
+     * @param max
+     */
+    public void setMaxRAM(int max) {
+        this.maxRAM = max;
+    }
+
+    public boolean saveMotherboard() {
+        Connection con = DatabaseConnection.establishConnection();
+
+        try {
+
+            String query = "INSERT INTO Part  (Price,Model,Make,PartType) VALUES (?,?,?,?)";
+
+            PreparedStatement statement = con.prepareStatement(query);
+
+            statement.setDouble(1, this.price);
+            statement.setString(2, this.model);
+            statement.setString(3, this.make);
+            statement.setString(4, "Motherboard");
+            statement.execute();
+            String model = this.model;
+            query = "SELECT * FROM Part WHERE Model ='" + model + "' && PartType = 'Motherboard'";
+
+            statement.executeQuery(query);
+            ResultSet rs = statement.getResultSet();
+            int partID = 0;
+            while (rs.next()) {
+                partID = rs.getInt("PartID");
+            }
+            query = "INSERT INTO Motherboard values (?,?,?,?,?)";
+            statement.setInt(1, partID);
+            statement.setString(2, this.socket);
+            statement.setString(3, this.size);
+            statement.setInt(4, this.ramSlots);
+             statement.setInt(5, this.maxRAM);
+            statement.execute();
+            return true;
+        } catch (SQLException err) {
+            System.out.println(err.getMessage());   //Prints out SQL error 
+            return false;
+
+        }
+
+    }
+
+}

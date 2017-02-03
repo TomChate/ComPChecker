b2164b9112558eff198eaa134dbc156c28c75838
lilliekhogg <lilliekhogg@gmail.com>
2017-02-03 11:51:19 +0000

    comments on make

--- src/Make.java
+++ src/Make.java
@@ -8,6 +8,7 @@
  *
  * @author Tom
  */
+//import needed packages
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -22,27 +23,27 @@ public class Make {
 
     public boolean checkMakes(String make) {
 
-        Connection con = DatabaseConnection.establishConnection();
+        Connection con = DatabaseConnection.establishConnection(); //connects to datbase
 
         try {
-            int availability;
+            int availability; //checks availibility using sql query
             Statement stmt = (Statement) con.createStatement();
-            String query = ("SELECT COUNT(*) FROM Make WHERE Name = '" + make + "'");
+            String query = ("SELECT COUNT(*) FROM Make WHERE Name = '" + make + "'"); //checks name of make in database
 
             stmt.executeQuery(query);
             ResultSet rs = stmt.getResultSet();
             while (rs.next()) {
-                availability = rs.getInt("COUNT(*)");
+                availability = rs.getInt("COUNT(*)"); //checks availibility of make in database
                 if (availability == 1) {
-                    return false;
+                    return false; // not in database already
                 } else {
-                    return true;
+                    return true; //already present in database
                 }
             }
 
         } catch (SQLException err) {
 
-            System.out.println(err.getMessage());
+            System.out.println(err.getMessage()); //error message displayed
             return false;
         }
         return false;
@@ -53,12 +54,12 @@ public class Make {
     Connection con = DatabaseConnection.establishConnection();
     
     try {
-       String query = "INSERT INTO Make values (?,?)"; 
+       String query = "INSERT INTO Make values (?,?)";  //inserts the inputs into database
           
        PreparedStatement statement = con.prepareStatement(query);
     
-       statement.setString(1,name);
-       statement.setString(2, website);
+       statement.setString(1,name); //insert name of part
+       statement.setString(2, website); //website of part
        statement.execute();
        JOptionPane.showMessageDialog(null, "Saved", "No Account Found", JOptionPane.INFORMATION_MESSAGE);
     }
@@ -68,28 +69,28 @@ public class Make {
      }
     
        public void setName(String name) {
-        this.name = name;
+        this.name = name; 
     }
        
        public void setWebsite(String website) {
         this.website = website;
     }
 
-       public ResultSet  getMakes(){
+       public ResultSet  getMakes(){ //returns the make to user
               Connection con = DatabaseConnection.establishConnection();
        try {
             int availability;
             Statement stmt = (Statement) con.createStatement();
-            String query = ("SELECT Name FROM Make ORDER BY Name;");
+            String query = ("SELECT Name FROM Make ORDER BY Name;"); //gets make from database refering to name
 
             stmt.executeQuery(query);
-            ResultSet rs = stmt.getResultSet();
+            ResultSet rs = stmt.getResultSet(); //returns the make
             return rs;
             
         } catch (SQLException err) {
 
             System.out.println(err.getMessage());
-            return null;
+            return null; //prints error message if not found
         }
        
        
